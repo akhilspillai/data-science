92ae8894bd5d92023bd380ea95b79449015f2438
Akhil S Pillai <akhil@revemarketing.com>
2021-07-21 17:57:30 +0530

    WIP: Before using tokenizer

---
+++ util.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def read_glove_vecs(glove_file):
+  with open(glove_file, 'r') as f:
+    words = set()
+    word_to_vec_map = {}
+    for line in f:
+      line = line.strip().split()
+      curr_word = line[0]
+      words.add(curr_word)
+      word_to_vec_map[curr_word] = np.array(line[1:], dtype=np.float64)
+    
+    i = 1
+    words_to_index = {}
+    index_to_words = {}
+    for w in sorted(words):
+      words_to_index[w] = i
+      index_to_words[i] = w
+      i = i + 1
+  return words_to_index, index_to_words, word_to_vec_map
+
+def format_data(x):
+  message = x["message"]
+  punct = [".", ",", "!", "?", "(", ")", ":"]
+  message = message.translate(str.maketrans({key: " {0} ".format(key) for key in punct}))
+  return message
+
+def is_not_in_glove(message, otp, word_to_vec_map):
+  for string in message:
+    if string != otp and string not in word_to_vec_map:
+      return True
+  return False
